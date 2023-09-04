83e5d88efbc973496a2f455cb347d1c9e5bed38a
ad-munir <mounir.ayad@etu.uae.ac.ma>
2023-09-04 23:38:04 +0100

    intial

---
+++ src/main/java/com/spring/auth/LoginrRegistrationV2Application.java
@@ -0,0 +1,13 @@
+package com.spring.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginrRegistrationV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginrRegistrationV2Application.class, args);
+    }
+
+}
